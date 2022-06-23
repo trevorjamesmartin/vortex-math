d7d8ffbf173acf32aed70d7ec4410e3ee0bf70d4
Trevor Martin <trevor.j.martin@gmail.com>
2022-06-23 19:56:54 -0400

    + resource links, ~ update handlers

--- src/App.tsx
+++ src/App.tsx
@@ -36,20 +36,34 @@ function genPerimeter(limit: number, diameter: number): Generator {
 }
 
 export default function App() {
-  const [color, setColor] = useState<any>('#000');
-  const [checked, setChecked] = useState<boolean>(false);
-  const [svgBackgroundColor, setSVGBackgroundColor] = useState<any>('transparent')
-  const [chart, setChart] = useState<any>([]);
-  const [modulus, setModulus] = useState<number>(9)//877
-  const [multiplier, setMultiplier] = useState<number>(2);//42
-  const [iterations, setIterations] = useState<number>(1);
-  const [start, setStart] = useState<number>(1);
-  const [log, setLog] = useState<any>([]);
+  const iterations = 1;
   const svgElement = useRef<any>(undefined);
   const bkgColorElement = useRef<any>(undefined);
+  const [start, setStart] = useState<number>(1);
+  const [color, setColor] = useState<any>('#000');
+  const [checked, setChecked] = useState<boolean>(false);
+  const [svgBackgroundColor, setSVGBackgroundColor] = useState<any>('transparent');
+  const [chart, setChart] = useState<any>([]);
+  const [log, setLog] = useState<any>([]);
+  const [modulus, setModulus] = useState<number>(9);
+  const [multiplier, setMultiplier] = useState<number>(2);
   const [spinVortex, setSpinVortex] = useState<boolean>(true);
+
   const getWidth = () => Math.max(1920, modulus);
-  const nextNumber = (n: number, base: number) => (n * multiplier) % base;
+
+  const nextNumber = (n: number, base: number, formula?: string) => {
+    switch (formula) {
+      case "try another formula?":
+        // todo...
+        break;
+      case `1 x ${multiplier}`:
+      case `1 * ${multiplier}`:
+      default:
+        return (n * multiplier) % base;
+    }
+    return n
+  }
+
   const vortexMath = (base: number) => {
     let points: any = [...genPerimeter(modulus, getWidth())];
     setChart(points);
@@ -98,71 +112,113 @@ export default function App() {
     let url = URL.createObjectURL(svgBlob);
     triggerDownload(url);
   }
+  const handleChangeN = (e: any) => {
+    if (Math.floor(e.target.value) < 1) {
+      return
+    }
+    switch (e.currentTarget.name) {
+      case 'multiplier':
+        setMultiplier(e.target.value);
+        break;
+      case 'modulus':
+        if (Math.floor(e.target.value) > Math.floor(start)) {
+          setModulus(e.target.value);
+        }
+        break;
+      case 'start':
+        if (Math.floor(modulus) > Math.floor(e.target.value)) {
+          setStart(e.target.value);
+        }
+        break;
+      default:
+        return
+    }
+  }
+  const handleChangeColor = (e: any) => {
+    setColor(e?.target?.value || '#000');
+  }
+  const handleChangeBackground = (e: any) => {
+    let background;
+    if (e.currentTarget.name === 'checkbox') {
+      const toggled = !checked;
+      setChecked(toggled);
+      background = toggled ? svgBackgroundColor : 'transparent';
+    } else {
+      background = checked ? e.target.value : '#000';
+    }
+    const body: any = document.querySelector("body");
+    setSVGBackgroundColor(background);
+    if (checked) {
+      body.style.backgroundColor = background;
+    }
+  }
   return <div className="app" style={{ color: color, backgroundColor: svgBackgroundColor }}>
     <div className="canvas-inputs">
       <div className="color-pickers">
         <label htmlFor="color">Line Color</label>
-        <input type="color" id="html5colorpicker" value={color} onChange={(e: any) => { setColor(e.target.value) }}></input>
+        <input type="color" id="html5colorpicker" value={color} onChange={handleChangeColor}></input>
         <div>
-          <input type="checkbox" checked={checked} onChange={(e: any) => {
-            const background = (!checked === false) ? 'transparent' : svgBackgroundColor;
-            setChecked(!checked);
-            setSVGBackgroundColor(background);
-            document.body.style.color = color;
-            document.body.style.background = background;
-            setTimeout(() => {
-              svgElement.current.focus();
-            }, 500);
-
-          }} />
-
+          <input type="checkbox" checked={checked} name="checkbox" onChange={handleChangeBackground} />
           <label htmlFor="backgroundColor">Background Color</label>
-          <input id="backgroundColor" type="color" ref={bkgColorElement} value={svgBackgroundColor} onChange={(e: any) => {
-            const background = checked ? e.target.value : 'transparent';
-
-            setSVGBackgroundColor(background);
-            document.body.style.background = background;
-
-          }}></input>
+          <input id="backgroundColor"
+            name="backgroundColor" type="color"
+            ref={bkgColorElement}
+            value={svgBackgroundColor}
+            onChange={handleChangeBackground} />
         </div>
       </div>
+      <div>
+        <label htmlFor="">Start</label>
+        <input type="number" value={start} name="start" onChange={handleChangeN}></input>
+      </div>
+      {/* Multiplier */}
       <div className="point-multiplier">
         <label htmlFor="multiplier">Multiplier</label>
         <input type="number" id="multiplier" name="multiplier"
-          min={2} value={multiplier}
-          onChange={(e: any) => {
-            setMultiplier(e.target.value);
-          }} />
+          min={1} value={multiplier}
+          onChange={handleChangeN} />
       </div>
-      <input type="range" value={multiplier} id="mult-slider" min="2" max="10000" onChange={(e: any) => setMultiplier(e.target.value)} />
+      <input type="range" value={multiplier} id="mult-slider" name="multiplier" min="2" max="10000" onChange={handleChangeN} />
+      {/* Modulus */}
       <div className="total-points">
         <label htmlFor="modulus">Modulus</label>
         <input type="number" id="modulus" name="modulus"
-          min={2} value={modulus}
-          onChange={(e: any) => {
-            setModulus(e.target.value);
-          }} />
+          min={start + 1} value={modulus}
+          onChange={handleChangeN} />
       </div>
-      <input type="range" value={modulus} id="mod-slider" min="2" max="10000" onChange={(e: any) => setModulus(e.target.value)} />
+      <input type="range" name="modulus" value={modulus} id="mod-slider" min="2" max="10000" onChange={handleChangeN} />
     </div>
+
     <button onClick={saveToFile}>save to file</button>
     <div className="svg-wrapper">
       <label htmlFor="spin-vortex">spin</label>
       <input id="spin-vortex" type="checkbox" checked={spinVortex} onChange={() => setSpinVortex(!spinVortex)} />
-
       <div className={spinVortex ? "svg-vortex" : "svg-wrapper"}>
         <svg viewBox={`0 0 ${getWidth()} ${getWidth()}`} ref={svgElement}>
           <circle cx={getWidth() / 2} cy={getWidth() / 2} r={getWidth() / 2} stroke={color} stroke-width="3" fill={svgBackgroundColor} />
-          <polyline points={
-            translateSVG()
-          } stroke-width="2" fill="none" stroke={color} />
+          <polyline points={translateSVG()} stroke-width="2" fill="none" stroke={color} />
           Sorry, your browser does not support inline SVG.
         </svg>
       </div>
+      </div>
+      <hr />
+      <label>About</label>
+      <div className="youtube-video">
+        <label htmlFor="vortex-math">Vortex Math</label>
+        <iframe name="vortex-math" width="560" height="315" src="https://www.youtube.com/embed/6ZrO90AI0c8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
 
-      <p hidden>
-        {log.join(', ')}
-      </p>
-    </div>
+      <div className="youtube-video">
+        <label htmlFor="generators">Javascript Generators</label>
+        <iframe name="generators" width="560" height="315" src="https://www.youtube.com/embed/gu3FfmgkwUc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+      <div className="source-code">
+        <a href="https://github.com/trevorjamesmartin/vortex-math" target="_new">Source Code</a>
+        <p hidden>
+          {log.join(', ')}
+        </p>
+      </div>
+
+
   </div>;
 }
